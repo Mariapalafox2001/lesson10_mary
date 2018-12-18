57392949be492a82d6e5c0220e8e79c23123ac7e
Mariapalafox2001 <43785186+Mariapalafox2001@users.noreply.github.com>
2018-12-18 10:12:20 -0500

    Add files via upload

---
+++ lesson10/Lesson10_problem_3.py
@@ -0,0 +1,11 @@
+print('-' * 60)
+print('Bouncer bot:')
+print()
+print('Welcome to CLUB425, the most lit club in downtown Actvf. Before you can enter, I need you to answer some questions?')
+age = input('What is your age today? ')
+if age == '13 <= 25':
+	print('cool come in')
+	print('alright, next come on people we do not have all night.')
+else:
+	print('Go home kid your too young to enter')
+	print('-' * 60)
---
+++ lesson10/lesson10_problem_1.py
@@ -0,0 +1,12 @@
+print('-' * 60)
+print('Life decision bot:')
+print()
+raining = input('Is it raining today? ')
+if raining == 'yes':
+	print('Stay home')
+	print('Maybe binge watch a new show Netflix.')
+else:
+	print('Go out and find a life!!!!!')
+	print('Or find yourself a job you lazy bum')
+	print('Or go for a hike.')
+	print('-' * 60)
